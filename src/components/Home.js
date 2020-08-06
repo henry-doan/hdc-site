faa129ceaee20abaa24f98c6213151d30f7b4288
Henry Doan <henrydoan96@gmail.com>
2020-08-05 20:03:59 -0600

    button structure

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Home = () => (
   <>
     Home
+    <a className='btn' href='/'>
+      <span className='top'></span>
+      <span></span>
+      <span className='bottom'></span>
+      <span></span>
+      Modules
+    </a>
   </>
 )
 
